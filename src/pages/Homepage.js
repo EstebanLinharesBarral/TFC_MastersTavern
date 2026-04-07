e41b6cfe97f7f62a023adcb5457244a4f981e3f5
EstebanLiBa <estebanliba@gmail.com>
2026-04-07 13:38:39 +0200

    Añadido FullCalendar

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,21 +1,46 @@
 // pages/Homepage.js
-
 function creadeDataCard() {
     return ``
 }
 
 export function renderHomepage() {
-    
+    setTimeout(() => {
+        
+            const calendarEl = document.getElementById('calendar');
+            
+            console.log(calendarEl)
+
+            if(calendarEl){
+                const calendar = new FullCalendar.Calendar(calendarEl, {
+                    initialView: 'dayGridMonth', // Vista inicial: mes
+                    headerToolbar: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                    }
+                });
+                calendar.render();
+            }
+
+    }, 1000)
+
     //SI NO HAY UN USUARIO CONECTADO CAMBIAR EL RETURN
-    return `<div class="grid grid-rows-10">
-        <div class="grid grid-cols-10 row-span-5">
-            <section class="col-span-6">
-                <div id="next-sessions-container">
+    return `<div class="grid grid-rows-10 pt-6 p-4">
+        <div class="grid grid-cols-10 row-span-5 gap-4">
+            <section class="col-span-4">
+                <div id="next-sessions-container" class="border text-center">
+                    <p>Próximas sesiones</p>
+                    <div class="grid grid-cols-2">
+
+                    </div>
                 </div>
                 <div id="prev-sessions-container">
+                    
                 </div>
             </section>
-            <section class="col-span-4"></section>
+            <section class="col-span-6">
+                <div id="calendar"></div>
+            </section>
         </div>
         <div class="row-span-5"></div>
     </div>`
