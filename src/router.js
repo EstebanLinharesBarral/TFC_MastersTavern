6f9857de2582b67d4119f97da39c6326959915fc
EsetbanLiBa <elinaresb24@fpcoruna.afundacion.org>
2026-04-12 12:15:45 +0200

    Empezando router

--- src/router.js
+++ src/router.js
@@ -1,3 +1,27 @@
+class Router {
+    constructor(routes){
+        this.routes = routes;
+        this.init();
+    }
+
+    init() {
+        window.addEventListener("hashchange", () => this.handleRouteChange());
+        this.handleRouteChange();
+    }
+
+    handleRouteChange() {
+        const currentPath = window.location.hash.slice(1);
+        const route = this.routes[currentPath];
+        if(route) {
+            route();
+        } else {
+            this.routes["/404"]();
+        }
+    }
+}
+
+
+
 export function createRouter(routes) {
 function router() {
     const path = window.location.pathname
